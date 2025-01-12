87eecb786b4905354a9df35a50a105315b65092a
valdadamva <149545238+valdadamva@users.noreply.github.com>
2025-01-12 21:39:55 +0300

    Valere_2D_reussi

--- Assets/Script/CamereMove2D.cs
+++ Assets/Script/CamereMove2D.cs
@@ -4,7 +4,6 @@ public class CameraSwitch : MonoBehaviour
 {
     public Camera camera1;  // Ссылка на первую камеру
     public Camera camera2;  // Ссылка на вторую камеру
-    public Vector3 targetPosition;  // Целевая позиция, при достижении которой происходит переключение
     public float switchThreshold = 1f; // Точность для достижения целевой позиции (погрешность)
 
     void Start()
@@ -17,10 +16,15 @@ public class CameraSwitch : MonoBehaviour
     void Update()
     {
         // Проверяем, достиг ли игрок целевой позиции
-        if (Vector3.Distance(transform.position, targetPosition) < switchThreshold)
+        if (transform.position.x > (float)11)
         {
             SwitchCameras();
         }
+		else
+		{
+			Start();
+		}
+ 
     }
 
     void SwitchCameras()
