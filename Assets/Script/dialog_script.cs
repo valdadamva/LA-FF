a30fbd6d4f8f7f176c97351cf5dc7093d3b9d002
valdadamva <149545238+valdadamva@users.noreply.github.com>
2025-03-15 03:14:12 +0300

    dialog_cree

---
+++ Assets/Script/dialog_script.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+using DialogueEditor;
+
+public class ConversationTest : MonoBehaviour
+{
+    [SerializeField] private NPCConversation myConversation;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+            }
+        }
+    }
+}
