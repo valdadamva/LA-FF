d0d3c16651c04c9506e19ee224bee3423e6fb23a
Nukver <130703617+Nukver@users.noreply.github.com>
2025-05-24 23:43:55 +0200

    Animation personnage
    Il peut tout faire juste le saut est buggé jdois le modifié

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -17,76 +17,93 @@ public class PlayerMovement : NetworkBehaviour
     public float groundCheckDistance = 0.1f;
 
     [Header("Network Sync")]
-    [SyncVar(hook = nameof(OnRunningChanged))] 
-    private bool _isRunning;
-    [SyncVar(hook = nameof(OnVelocityChanged))]
-    private Vector3 _syncVelocity;
-    [SyncVar(hook = nameof(OnRotationChanged))]
-    private Quaternion _syncRotation;
+    [SyncVar(hook = nameof(OnRunningChanged))] private bool _isRunning;
+    [SyncVar(hook = nameof(OnWalkingChanged))] private bool _isWalking;
+    [SyncVar(hook = nameof(OnVelocityChanged))] private Vector3 _syncVelocity;
+    [SyncVar(hook = nameof(OnRotationChanged))] private Quaternion _syncRotation;
 
     private Rigidbody _rigidbody;
     private bool _isGrounded;
-    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+    private Animator animator;
+    private bool movementEnabled = true;
+    public List<System.Func<float>> speedOverrides = new();
 
     public bool IsRunning => _isRunning;
-    private bool movementEnabled = true;
 
     void Awake()
     {
         InitializeRigidbody();
     }
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void InitializeRigidbody()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        if (_rigidbody == null)
-        {
-            Debug.LogError("Rigidbody component is missing!", this);
-            enabled = false;
-            return;
-        }
-
         _rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
         _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
     void Update()
     {
-        if (!isLocalPlayer) return;
-        if (!movementEnabled) return;
+        if (!isLocalPlayer || !movementEnabled) return;
 
+        CheckGrounded();
         HandleRunningInput();
         HandleJumpInput();
+        HandleWalkingInput();
+    }
+
+    private void HandleWalkingInput()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        bool walking = vertical > 0.1f;
+        if (_isWalking != walking)
+        {
+            CmdSetWalking(walking);
+        }
     }
 
     private void HandleRunningInput()
     {
-        bool newRunningState = canRun && Input.GetKey(runningKey);
-        if (_isRunning != newRunningState)
+        bool runningState = canRun && Input.GetKey(runningKey);
+        if (_isRunning != runningState)
         {
-            CmdSetRunning(newRunningState);
+            CmdSetRunning(runningState);
         }
     }
 
     private void HandleJumpInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            CmdJump();
+            PerformJump();
+            CmdNotifyJump();
         }
     }
 
-    [Command]
-    private void CmdSetRunning(bool runningState)
+    private void PerformJump()
     {
-        _isRunning = runningState;
+        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        if (animator)
+        {
+            animator.SetTrigger("JumpUp");
+            animator.SetBool("IsGrounded", false);
+            animator.SetBool("IsJumping", true);
+        }
+        _isGrounded = false;
     }
 
     [Command]
-    private void CmdJump()
-    {
-        RpcJump();
-    }
+    private void CmdSetWalking(bool walking) => _isWalking = walking;
+
+    [Command]
+    private void CmdSetRunning(bool running) => _isRunning = running;
+
+    private void CmdNotifyJump() => RpcJump();
 
     [ClientRpc]
     private void RpcJump()
@@ -95,24 +112,41 @@ public class PlayerMovement : NetworkBehaviour
         {
             _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        if (animator != null)
+        {
+            animator.SetTrigger("JumpUp");
+            animator.SetBool("IsGrounded", false);
+            animator.SetBool("IsJumping", true);
+        }
     }
 
     private void CheckGrounded()
     {
-        float sphereRadius = 0.4f;
-        float checkDistance = 0.3f;
+        Vector3 origin = transform.position + Vector3.up * 0.3f;
+        bool wasGrounded = _isGrounded;
 
         _isGrounded = Physics.SphereCast(
-            transform.position + Vector3.up * 0.1f,
-            sphereRadius,
+            origin,
+            0.2f,
             Vector3.down,
             out RaycastHit hit,
-            checkDistance,
+            0.5f,
             groundLayer
         );
 
-        Debug.DrawRay(transform.position, Vector3.down * (checkDistance + 0.1f), 
-            _isGrounded ? Color.green : Color.red);
+        if (animator != null)
+        {
+            animator.SetBool("IsGrounded", _isGrounded);
+
+            if (!wasGrounded && _isGrounded)
+            {
+                animator.SetTrigger("JumpDown");
+                animator.SetBool("IsJumping", false);
+            }
+        }
+
+        Debug.DrawRay(transform.position, Vector3.down * 1f, _isGrounded ? Color.green : Color.red, 1f);
     }
 
 #if UNITY_EDITOR
@@ -150,30 +184,19 @@ public class PlayerMovement : NetworkBehaviour
         float speed = GetCurrentSpeed();
         Vector2 input = GetMovementInput();
 
-        Vector3 velocity = transform.TransformDirection(new Vector3(
-            input.x * speed,
-            _rigidbody.linearVelocity.y,
-            input.y * speed
-        ));
+        Vector3 moveDir = transform.TransformDirection(new Vector3(input.x, 0, input.y));
+        Vector3 velocity = moveDir * speed;
+        velocity.y = _rigidbody.linearVelocity.y;
 
         _rigidbody.linearVelocity = velocity;
         CmdUpdateMovement(new Vector2(velocity.x, velocity.z), transform.rotation);
     }
 
-    private float GetCurrentSpeed()
-    {
-        if (speedOverrides.Count > 0)
-            return speedOverrides[speedOverrides.Count - 1]();
-        return _isRunning ? runSpeed : moveSpeed;
-    }
+    private float GetCurrentSpeed() =>
+        speedOverrides.Count > 0 ? speedOverrides[^1]() : (_isRunning ? runSpeed : moveSpeed);
 
-    private Vector2 GetMovementInput()
-    {
-        return new Vector2(
-            Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical")
-        );
-    }
+    private Vector2 GetMovementInput() =>
+        new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
     [Command]
     private void CmdUpdateMovement(Vector2 horizontalVelocity, Quaternion rotation)
@@ -205,14 +228,24 @@ public class PlayerMovement : NetworkBehaviour
     public void SetMovementEnabled(bool isEnabled)
     {
         movementEnabled = isEnabled;
-
         if (!isEnabled && _rigidbody != null)
         {
             _rigidbody.linearVelocity = new Vector3(0, _rigidbody.linearVelocity.y, 0);
         }
     }
 
-    private void OnRunningChanged(bool oldValue, bool newValue) => _isRunning = newValue;
+    private void OnRunningChanged(bool oldValue, bool newValue)
+    {
+        _isRunning = newValue;
+        if (animator) animator.SetBool("isRunning", newValue);
+    }
+
+    private void OnWalkingChanged(bool oldValue, bool newValue)
+    {
+        _isWalking = newValue;
+        if (animator) animator.SetBool("isWalking", newValue);
+    }
+
     private void OnVelocityChanged(Vector3 oldValue, Vector3 newValue) => _syncVelocity = newValue;
     private void OnRotationChanged(Quaternion oldValue, Quaternion newValue) => _syncRotation = newValue;
 }
